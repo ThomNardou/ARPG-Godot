8618615509d5d479d2fedb5d202e240faf0551ca
ReactedTitan880 <thomasnardou@icloud.com>
2023-11-13 19:15:15 +0100

    add attack system

--- player.gd
+++ player.gd
@@ -1,5 +1,17 @@
 extends CharacterBody2D
 
+var globale = Global
+
+var enemy_attack_range = false
+var enemy_attack_cooldown = true
+
+var health = 100
+var player_alive = true
+
+var direction = "none"
+
+var attack_ip = false
+
 @export var speed = 35
 @onready var animation = $AnimatedSprite2D
 
@@ -13,14 +25,18 @@ func  updateAnimation():
 		animation.stop()	
 	
 	else :
-		var direction = "up"
+
+		direction = "up"
 	
 		if velocity.x < 0: 
-			direction = "walk"
+			if !attack_ip:
+				direction = "walk"
 		elif velocity.x > 0: 
-			direction = "walk"
+			if !attack_ip:
+				direction = "walk"
 		elif velocity.y < 0:
-			direction = "up"
+			if !attack_ip:
+				direction = "up"
 			
 		if velocity.x != 0:
 			$AnimatedSprite2D.flip_h = velocity.x < 0
@@ -28,7 +44,58 @@ func  updateAnimation():
 		
 
 func  _physics_process(delta):
-	
 	handleInpute()
 	move_and_slide()
 	updateAnimation()
+	
+	enemyAttack()
+	attack()
+	
+	if  health <= 0:
+		player_alive = false
+		health = 0
+		print("tu es mort")
+		hide()
+	
+
+func player():
+	pass
+
+func _on_player_hit_box_body_entered(body):
+	if body.has_method("enemy"):
+		enemy_attack_range = true
+
+
+func _on_player_hit_box_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_attack_range = false
+
+func enemyAttack():
+	if enemy_attack_range and enemy_attack_cooldown == true:
+		health -= 10
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+
+func _on_attack_cooldown_timeout():
+	enemy_attack_cooldown = true
+	
+func attack():
+	var dir = direction
+	
+	if Input.is_action_just_pressed("attack"):
+		globale.player_current_attack = true
+		attack_ip = true
+		
+		if dir == "walk":
+			$deal_attack_timer.start()
+		
+		if dir == "up":
+			$deal_attack_timer.start()
+
+
+func _on_deal_attack_timer_timeout():
+	$deal_attack_timer.stop()
+	globale.player_current_attack = false
+	attack_ip = false
--- slime.gd
+++ slime.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
 
+var global = Global
+
 var speed = 40
 var player_chase = false
 var player = null
 
+var can_take_damage = true
+
+var health = 100
+var player_attack_zone = false
+
 func _physics_process(delta):
+	deal_with_damage()
+	
 	if  player_chase:
 		position += (player.position - position) / speed
 	
@@ -24,3 +33,30 @@ func _on_detection_area_body_entered(body):
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
+
+func enemy():
+	pass
+
+
+func _on_enemy_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_attack_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_attack_zone = false
+		
+func deal_with_damage():
+	if player_attack_zone and global.player_current_attack == true:
+		if can_take_damage:
+			health -= 10
+			$takeDamage.start()
+			can_take_damage = false
+			print("vie du slime :", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_timeout():
+	can_take_damage = true

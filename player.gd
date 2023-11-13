41f0175b77a243438d482f3a8761fb13b66ef5c1
ReactedTitan880 <thomasnardou@icloud.com>
2023-11-13 15:25:47 +0100

    change player movement and add sprite colision

--- player.gd
+++ player.gd
@@ -1,39 +1,34 @@
-extends Area2D
-signal  hit
+extends CharacterBody2D
 
-@export var speed = 400
-var screen_size
+@export var speed = 35
+@onready var animation = $AnimatedSprite2D
 
-func _ready():
-	screen_size = get_viewport_rect().size
-
-func _process(delta):
-	var  velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-		
-	else: 
-		$AnimatedSprite2D.stop()
-
-	position += velocity * delta
+func handleInpute():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * speed
 	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
+func  updateAnimation():
 	
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
+	if velocity.length() == 0:
+		animation.stop()	
+	
+	else :
+		var direction = "up"
+	
+		if velocity.x < 0: 
+			direction = "walk"
+		elif velocity.x > 0: 
+			direction = "walk"
+		elif velocity.y < 0:
+			direction = "up"
+			
+		if velocity.x != 0:
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		
+	
+		animation.play(direction)
+
+func  _physics_process(delta):
+	handleInpute()
+	move_and_slide()
+	updateAnimation()
